3824e4c7f4eca2cc4313e9f4f40a9dc57fb99a11
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-13 14:47:27 -0700

    The last day

--- src/section4/DragonWarsiorses.java
+++ src/section4/DragonWarsiorses.java
@@ -6,40 +6,51 @@ import javax.swing.JOptionPane;
 
 public class DragonWarsiorses {
 	public static void main(String[] args) {
-		int health = 75;
-		int dragon = 100;
+		int health = 150;
+		int dragon = 200;
 		int mana = 50;
 		int selected = 0;
 		int randamage = 0;
-		int bars = 2;
+		int bars = 3;
 		int stun = 0;
-		Object attacks[] = {"Bash (-5 Mana)", "Blitz (-15 Mana)", "Candy Bar (Restore Mana)"};
+		Object attacks[] = {"Bash", "Blitz (-10 Mana)", "Annihilate (-30 Mana)","Candy Bar (Restore Mana)"};
+		Object diffs[] = {"Easy", "Medium", "Hard"};
+		int difficulty = JOptionPane.showOptionDialog(null, "Select your difficulty!", "Difficulty", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, diffs, null);
+		if (difficulty == 0) {
+			health = 75;
+			dragon = 100;
+			bars = 2;
+		} else if (difficulty == 1) {
+			health = 150;
+			dragon = 200;
+			bars = 3;
+		} else {
+			health = 300;
+			dragon = 400;
+			bars = 5;
+		}
+		int maxhealth = health;
+		int maxdragon = dragon;
 		Random RNGsus = new Random();
 		JOptionPane.showMessageDialog(null, "You're exploring a mysterious cave, when you stumble upon a vast pile of glittering treasure.");
 		JOptionPane.showMessageDialog(null, "As you stand there, shocked and surprised by your extremely lucky find, you almost miss the roar of a dragon.");
 		while (health > 0 && dragon > 0) {
-			selected = JOptionPane.showOptionDialog(null, "You are fighting a dragon!\n\nYour Health: " + health + "/75\nYour Mana: " + mana + "/50\n-----\nDragon Health: " + dragon + "/100\n\nCandy Bars: " + bars, "Battle", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, attacks, null);
+			selected = JOptionPane.showOptionDialog(null, "You are fighting a dragon!\n\nYour Health: " + health + "/" + maxhealth + "\nYour Mana: " + mana + "/50\n-----\nDragon Health: " + dragon + "/" + maxdragon + "\n\nCandy Bars: " + bars, "Battle", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, attacks, null);
 			if (selected == 0) {
-				if (mana >= 5) {
-					dragon -= 5;
-					mana -= 5;
-					stun = RNGsus.nextInt(5);
-					if (stun == 0) {
-						JOptionPane.showMessageDialog(null, "Noticing an opportunity, you aim your club and leap towards the dragon right as it exposes its weak underbelly. It takes 5 damage, and falls to the ground, immobilized.");
-						stun = 10;
-					} else {
-						JOptionPane.showMessageDialog(null, "You grab your club and bash it with your hammer as hard as you can. You notice a slight grimace as it takes 5 damage.");
-					}
+				dragon -= 5;
+				stun = RNGsus.nextInt(5);
+				if (stun == 0) {
+					JOptionPane.showMessageDialog(null, "Noticing an opportunity, you aim your club and leap towards the dragon right as it exposes its weak underbelly. It takes 5 damage, and falls to the ground, immobilized.");
+					stun = 10;
 				} else {
-					selected = -1;
-					JOptionPane.showMessageDialog(null, "You don't have enough mana!");
+					JOptionPane.showMessageDialog(null, "You grab your club and bash it with your hammer as hard as you can. You notice a slight grimace as it takes 5 damage.");
 				}
 			}
 			if (selected == 1) {
-				if (mana >= 15) {
+				if (mana >= 10) {
 					randamage = RNGsus.nextInt(30) + 1;
 					dragon -= randamage;
-					mana -= 15;
+					mana -= 10;
 					JOptionPane.showMessageDialog(null, "You focus your will on destroying this vile beast, and launch yourself into the dragon. It recoils, taking " + randamage + " damage.");
 				} else {
 					selected = -1;
@@ -47,6 +58,19 @@ public class DragonWarsiorses {
 				}
 			}
 			if (selected == 2) {
+				if (mana >= 30) {
+					mana -= 30;
+					JOptionPane.showMessageDialog(null, "You ready your sword...");
+					for (int attc = RNGsus.nextInt(10) + 1; attc > 0; attc--) {
+						dragon -= 5;
+						JOptionPane.showMessageDialog(null, "You slash the dragon, dealing 5 damage!");
+					}
+				} else {
+					selected = -1;
+					JOptionPane.showMessageDialog(null, "You don't have enough mana!");
+				}
+			}
+			if (selected == 3) {
 				if (bars > 0 && mana < 50) {
 					mana = 50;
 					bars -= 1;
@@ -75,7 +99,7 @@ public class DragonWarsiorses {
 				}
 			}
 		}
-		if (health == 0) {
+		if (health <= 0) {
 			JOptionPane.showMessageDialog(null, "Overwhelmed by your wounds, you stumble to the floor. If only there was a way to try again, you think, as you take your dying breath...\n\nGAME OVER");
 		} else {
 			JOptionPane.showMessageDialog(null, "The dragon is becoming too weak to defend itself, and you run up to it and slice off its head! As it collapses, you begin wondering how you're going to move all this treasure...\n\nYOU WIN");
--- src/section5/ScaryMaze.java
+++ src/section5/ScaryMaze.java
@@ -19,41 +19,55 @@ import javax.swing.SwingUtilities;
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	
 	BufferedImage maze;
+	BufferedImage maze2;
+	BufferedImage maze3;
 	final int frameWidth = 800;
 	final int frameHeight = 600;
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your section5 package: http://pixlr.com/editor/
-		//maze = ImageIO.read(getClass().getResource("standardMaze.jpg"));
+		maze = ImageIO.read(getClass().getResource("maze.png"));
+		maze2 = ImageIO.read(getClass().getResource("maze2.png"));
+		maze3 = ImageIO.read(getClass().getResource("maze3.png"));
 		//2. Change the line of code above so that it matches your maze's file name
 		
 		//3. Set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(45, 95);
+		new Robot().mouseMove(100, 100);
 		
 		//4. Add a mouse motion listener using:
 		//addMouseMotionListener(this);
-		
+		addMouseMotionListener(this);
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
+		int curmaze = 0;
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//5. Print the mouseColor variable 
-		
 		//6.  Run your program and put your mouse over the background to find out what color it is
 		
 		//7. Create an int variable that holds the background color. 
-		
+		int bg = -16777216;
 		//8. If the mouse falls off the path (if it is on the background)
-	
+		if (mouseColor == bg) {
 				// call the scare method
-		
+			scare();
+			JOptionPane.showMessageDialog(null, "brush ur teeth ;)");
+		}
 		//13. If the mouse is on the end color
-		
-				// pop up a message to tell them they won
-		
+		if (mouseColor == -1900544) {
+			if (curmaze == 0) {
+				maze = maze2;
+				curmaze++;
+			} else if (curmaze == 1) {
+				maze = maze3;
+				curmaze++;
+			} else {
+				JOptionPane.showMessageDialog(null, "You win!");
+			}
+		}
 	}
 
 	private void scare() {
@@ -61,12 +75,12 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		//9. Find a scary sound and put it in the section5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
 		
 		//10. Use the code below to load your sound.  Change the file name to match the name of your sound file.  
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("standardScarySound.wav"));
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("demon.wav"));
 		
 		//11. Play the scary sound. Hint: type "sound" and then a period.		
-		
+		sound.play();
 		//12. Drop an image into your section5 package, and use the showScaryImage method to scare your victim!
-		
+		showScaryImage("spookd.jpg");
 	}
 
 	private void showScaryImage(String imageName) {
